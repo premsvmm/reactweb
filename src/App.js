5c2a475b136bbe9abf7068744d165ef4a490c6c9
Premkumar <prem.kumar@razorpay.com>
2020-04-26 16:23:10 +0530

    added missed file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
 const [hostname,setHostname]=useState(null);
 useEffect(()=>{
-  fetch("http://localhost:8080/hostname")
+  fetch("http://webinar.com/hostname")
   .then(data=>data.json())
   .then(data=>{
     setHostname(data.host_name)
