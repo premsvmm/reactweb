5c4d02e33e953776f31f807cd488efaefd567656
Premkumar <prem.kumar@razorpay.com>
2020-04-26 14:58:21 +0530

    Call host api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [hostname,setHostname]=useState(null);
+useEffect(()=>{
+  fetch("http://localhost:8080/hostname")
+  .then(data=>data.json())
+  .then(data=>{
+    setHostname(data.host_name)
+  });
+},[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            hostname?<h1 style={{fontSize:'100px'}}>{hostname}</h1>:null
+          }
       </header>
     </div>
   );
